1946e4f105bb793a9be64aa5d69b9fe9998334f8
Zamua Halera <zamuahalera@macbook-pro-de-zamua.home>
2023-10-27 23:06:30 +0200

    correction mailer

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/form", async (req, res) => {
       messageData
     );
     console.log(req.body);
-    res.status(200).json(firstname);
+    res.status(200).json(response);
   } catch (error) {
     res.status(500).json({
       message: error.message,
